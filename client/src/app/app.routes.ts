883e73b069c55a5d8d6c1af98ed2bc76b4e15a65
Pablo Valenzuela <pabloeng05@gmail.com>
2024-04-18 20:25:38 -0600

    feat: Login Card Main Components Added +Login Card and Login Page basic blocks created (components, services, etc).

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login',title:"LogIn Page", component: LoginPageComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
---
+++ client/src/app/auth/components/login-card/login-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-card.component.html',
+  styleUrl: './login-card.component.scss'
+})
+export class LoginCardComponent {
+  login() {}
+}
