3d337dc04e4cf22f35d55b15e08004c42d9c20c7
Pablo Valenzuela <pabloeng05@gmail.com>
2024-04-19 10:32:38 -0600

    feat: user-profile Page Added +Skeleton component added

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
+import { UserProfilePageComponent } from './user-profile/pages/user-profile-page/user-profile-page.component';
 
 export const routes: Routes = [
     { path: 'login',title:"LogIn Page", component: LoginPageComponent },
+    { path: 'profile',title:"Profile Page", component: UserProfilePageComponent },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
